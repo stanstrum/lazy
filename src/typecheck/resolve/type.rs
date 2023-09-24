4dd7327d7a14e1a009304a6d8f67de82b9890fe4
Stan Strum <stanleystrum@gmail.com>
2023-09-23 21:23:52 -0700

    Fix type resolver not actually overwriting unknown `Type`s
    The problem here is I was cloning the destination to avoid a borrowck
    error despite the fact I needed the original reference to overwrite.
    Oops.

--- src/typecheck/resolve/type.rs
+++ src/typecheck/resolve/type.rs
@@ -9,80 +9,93 @@ use super::*;
 
 impl Checker {
   pub fn resolve_type(&mut self, ast: &mut TypeAST) -> TypeCheckResult<()> {
-    match &mut ast.e.clone() {
-      Type::Intrinsic(_)
-      | Type::Defined(_)
-      | Type::Function(_) => Ok(()),
-      Type::ConstReferenceTo(ast)
-      | Type::MutReferenceTo(ast)
-      | Type::ConstPtrTo(ast)
-      | Type::MutPtrTo(ast) => {
-        self.resolve_type(ast)
-      },
-      Type::ArrayOf(lit, ast) => {
-        let lit = lit.as_ref();
+    let what_to_replace_with = 'replace_with: {
+      match &mut ast.e {
+        Type::Intrinsic(_)
+        | Type::Defined(_)
+        | Type::Function(_) => {
+          return Ok(());
+        },
+        Type::ConstReferenceTo(ast)
+        | Type::MutReferenceTo(ast)
+        | Type::ConstPtrTo(ast)
+        | Type::MutPtrTo(ast) => {
+          self.resolve_type(ast);
 
-        if lit.is_some_and(|lit| !is_valid_array_length(&lit)) {
-          return InvalidTypeSnafu {
-            text: "Array length is invalid",
-            span: lit.unwrap().span.clone()
-          }.fail();
-        };
+          return Ok(());
+        },
+        Type::ArrayOf(lit, ast) => {
+          let lit = lit.as_ref();
 
-        self.resolve_type(&mut **ast)
-      },
-      Type::Unknown(qual) => {
-        let mut res_stack: Vec<_> = self.stack.iter()
-          .filter_map(|scope|
-            match scope {
-              | ScopePointer::Function(_)
-              | ScopePointer::Expression(_)
-              | ScopePointer::Block(_) => None,
-              ScopePointer::Namespace(ns) =>
-              Some(*ns)
+          if lit.is_some_and(|lit| !is_valid_array_length(&lit)) {
+            return InvalidTypeSnafu {
+              text: "Array length is invalid",
+              span: lit.unwrap().span.clone()
+            }.fail();
+          };
+
+          self.resolve_type(&mut **ast)?;
+
+          return Ok(());
+        },
+        Type::Unknown(qual) => {
+          let mut res_stack: Vec<_> = self.stack.iter()
+            .filter_map(|scope|
+              match scope {
+                | ScopePointer::Function(_)
+                | ScopePointer::Expression(_)
+                | ScopePointer::Block(_) => None,
+                ScopePointer::Namespace(ns) =>
+                Some(*ns)
+              }
+            ).collect();
+
+          let last_idx = qual.parts.len() - 1;
+          let part_iter = qual.parts
+            .iter()
+            .enumerate()
+            .map(
+              |(i, ident)|
+                (i == last_idx, ident.text.to_owned())
+            );
+
+          for (is_last, part) in part_iter {
+            let map = unsafe { &mut (**res_stack.last().unwrap()).map };
+
+            match (is_last, map.get_mut(&part)) {
+              (false, Some(Structure::Namespace(ns))) => {
+                res_stack.push(&mut *ns);
+              },
+              (false, _) if part == "super" => {
+                res_stack.pop();
+              },
+              (true, Some(Structure::TypeAlias(alias))) => {
+                self.resolve_type(&mut alias.ty)?;
+
+                break 'replace_with Type::Defined(&alias.ty);
+              },
+              (true, Some(Structure::Function(func))) => {
+                break 'replace_with Type::Function(func);
+              },
+              _ => {
+                return UnknownIdentSnafu {
+                  text: qual.to_hashable(),
+                  span: qual.span.clone()
+                }.fail();
+              }
             }
-          ).collect();
+          };
 
-        let last_idx = qual.parts.len() - 1;
-        let part_iter = qual.parts
-          .iter()
-          .enumerate()
-          .map(
-            |(i, ident)|
-              (i == last_idx, ident.text.to_owned())
-          );
+          return Ok(());
+        },
+        Type::Unresolved => todo!("type alias dest unresolved"),
+      };
 
-        for (is_last, part) in part_iter {
-          let map = unsafe { &mut (**res_stack.last().unwrap()).map };
+    };
 
-          match (is_last, map.get_mut(&part)) {
-            (false, Some(Structure::Namespace(ns))) => {
-              res_stack.push(&mut *ns);
-            },
-            (false, _) if part == "super" => {
-              res_stack.pop();
-            },
-            (true, Some(Structure::TypeAlias(alias))) => {
-              self.resolve_type(&mut alias.ty)?;
+    ast.e = what_to_replace_with;
 
-              ast.e = Type::Defined(&alias.ty);
-            },
-            (true, Some(Structure::Function(func))) => {
-              ast.e = Type::Function(func);
-            },
-            _ => {
-              return UnknownIdentSnafu {
-                text: qual.to_hashable(),
-                span: qual.span.clone()
-              }.fail();
-            }
-          }
-        };
-
-        Ok(())
-      },
-      Type::Unresolved => todo!("type alias dest unresolved"),
-    }
+    Ok(())
   }
 
   pub fn resolve_alias(&mut self, alias: &mut TypeAliasAST) -> TypeCheckResult<()> {

c91784533d32b8ced8222f213c6c5530447b8246
Stan Strum <stanleystrum@gmail.com>
2023-10-07 10:24:33 -0700

    aster: split apart ast.rs

---
+++ src/aster/ast/expression.rs
@@ -0,0 +1,143 @@
+/* Copyright (c) 2023 Stan Strum
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+use super::*;
+use crate::make_get_span;
+
+use std::collections::HashMap;
+
+#[derive(Debug, Clone, Hash, PartialEq, Eq)]
+pub enum VariableReference {
+  Unresolved,
+  ResolvedVariable(*const BindingAST),
+  ResolvedArgument(*const TypeAST),
+  ResolvedFunction(*const FunctionAST),
+  ResolvedMemberFunction(*const MemberFunctionAST),
+  #[allow(unused)]
+  ResolvedMemberOf(*const VariableReference, *const IdentAST)
+}
+
+#[derive(Debug, Clone)]
+pub enum AtomExpression {
+  Literal(LiteralAST),
+  Variable(QualifiedAST, VariableReference),
+  Return(Option<BoxExpr>),
+  #[allow(unused)]
+  Break(Option<BoxExpr>),
+}
+
+#[derive(Debug, Clone)]
+pub struct AtomExpressionAST {
+  pub span: Span,
+  pub out: Type,
+  pub a: AtomExpression,
+}
+
+#[derive(Debug, Clone)]
+pub struct SubExpressionAST {
+  pub span: Span,
+  pub out: Type,
+  pub e: BoxExpr
+}
+
+#[derive(Debug, Clone)]
+pub struct BindingAST {
+  pub span: Span,
+
+  pub r#mut: Option<KeywordAST>,
+  pub ty: Option<TypeAST>,
+  pub ident: IdentAST,
+  pub value: Option<BoxExpr>
+}
+
+#[derive(Debug, Clone)]
+pub enum BlockExpressionChild {
+  Binding(BindingAST),
+  Expression(Expression)
+}
+
+#[derive(Debug, Clone)]
+pub struct BlockExpressionAST {
+  pub span: Span,
+  pub out: Type,
+  pub children: Vec<BlockExpressionChild>,
+  pub vars: HashMap<IdentAST, *mut BindingAST>,
+  pub returns_last: bool,
+}
+
+#[derive(Debug, Clone)]
+pub enum ControlFlow {
+  If(
+    Vec<
+      (Expression, BlockExpressionAST)
+    >,
+    Option<BlockExpressionAST>
+  ),
+  While(
+    BoxExpr,
+    Box<BlockExpressionAST>
+  ),
+  #[allow(unused)]
+  DoWhile(
+    Box<BlockExpressionAST>,
+    BoxExpr
+  ),
+  Loop(Box<BlockExpressionAST>),
+  // For(
+  //   CondExpr, ElseBranch
+  // ),
+}
+
+#[derive(Debug, Clone)]
+pub struct ControlFlowAST {
+  pub span: Span,
+  pub e: ControlFlow
+}
+
+#[derive(Debug, Clone)]
+pub enum Expression {
+  Atom(AtomExpressionAST),
+  Block(BlockExpressionAST),
+  SubExpression(SubExpressionAST),
+  ControlFlow(ControlFlowAST),
+  UnaryOperator(UnaryOperatorExpressionAST),
+  BinaryOperator(BinaryOperatorExpressionAST),
+}
+
+impl GetSpan for BlockExpressionChild {
+  fn span(&self) -> Span {
+    match self {
+      BlockExpressionChild::Binding(binding) => binding.span(),
+      BlockExpressionChild::Expression(expr) => expr.span(),
+    }
+  }
+}
+
+impl GetSpan for SubExpressionAST {
+  fn span(&self) -> Span {
+    GetSpan::span(&*self.e)
+  }
+}
+
+impl GetSpan for Expression {
+  fn span(&self) -> Span {
+    match self {
+      Expression::Atom(s) => s.span(),
+      Expression::Block(s) => s.span(),
+      Expression::SubExpression(s) => s.span(),
+      Expression::ControlFlow(s) => s.span(),
+      Expression::BinaryOperator(s) => s.span(),
+      Expression::UnaryOperator(s) => s.span()
+    }
+  }
+}
+
+make_get_span![
+  BindingAST,
+  ControlFlowAST,
+  UnaryOperatorExpressionAST
+];
---
+++ src/aster/ast/type.rs
@@ -0,0 +1,111 @@
+/* Copyright (c) 2023 Stan Strum
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+use super::*;
+use crate::make_get_span;
+
+use crate::aster::intrinsics::Intrinsic;
+
+#[derive(Debug, Clone)]
+pub enum Literal {
+  UnicodeString(String),
+  ByteString(String),
+  CString(String),
+  Char(char),
+  ByteChar(char),
+  FloatLiteral(String),
+  IntLiteral(String),
+}
+
+#[derive(Debug, Clone)]
+pub struct LiteralAST {
+  pub span: Span,
+  pub l: Literal,
+}
+
+#[allow(unused)]
+#[derive(Debug, Clone)]
+pub enum Type {
+  Intrinsic(Intrinsic),
+  Function(*const FunctionAST),
+  Struct(*const StructAST),
+  ConstReferenceTo(Box<TypeAST>),
+  MutReferenceTo(Box<TypeAST>),
+  ConstPtrTo(Box<TypeAST>),
+  MutPtrTo(Box<TypeAST>),
+  ArrayOf(Option<LiteralAST>, Box<TypeAST>),
+  Defined(*const TypeAST),
+  Unknown(QualifiedAST),
+  UnresolvedLiteral(Literal),
+  Unresolved
+}
+
+#[derive(Debug, Clone)]
+pub struct TypeAST {
+  pub span: Span,
+  pub e: Type,
+}
+
+impl LiteralAST {
+  pub fn to_hashable(&self) -> String {
+    match &self.l {
+      Literal::FloatLiteral(text) => text.to_owned(),
+      Literal::IntLiteral(text) => text.to_owned(),
+      _ => panic!("to_hashable run on non-numeric")
+    }
+  }
+}
+
+impl Type {
+  pub fn to_hashable(&self) -> String {
+    match self {
+      Type::Intrinsic(s) => {
+        s.get_name()
+      },
+      Type::ConstReferenceTo(ty) => {
+        format!("&{}", ty.e.to_hashable())
+      },
+      Type::MutReferenceTo(ty) => {
+        format!("&mut {}", ty.e.to_hashable())
+      },
+      Type::ConstPtrTo(ty) => {
+        format!("*{}", ty.e.to_hashable())
+      },
+      Type::MutPtrTo(ty) => {
+        format!("*mut {}", ty.e.to_hashable())
+      },
+      Type::ArrayOf(sz, ty) => {
+        match sz {
+          Some(sz) => {
+            format!("[{}]{}", sz.to_hashable(), ty.e.to_hashable())
+          },
+          None => {
+            format!("[]{}", ty.e.to_hashable())
+          },
+        }
+      },
+      Type::Defined(ty) => unsafe {
+        (**ty).e.to_hashable()
+      },
+      Type::Unknown(qual) => {
+        qual.to_hashable()
+      },
+      _ => unimplemented!("to_hashable for {:#?}", self)
+    }
+  }
+}
+
+impl TypeAST {
+  pub fn to_hashable(&self) -> String {
+    self.e.to_hashable()
+  }
+}
+
+make_get_span![
+  TypeAST,
+  LiteralAST
+];

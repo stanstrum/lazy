37c5e40974cc69c2b796ce0e1e0b22d432974331
Stan Strum <stanleystrum@gmail.com>
2023-10-07 15:18:08 -0700

    resolve: ExternDeclAST in resolve_ns

--- src/typecheck/resolve/namespace.rs
+++ src/typecheck/resolve/namespace.rs
@@ -53,7 +53,13 @@ impl Checker {
           self.resolve_impl(r#impl)?;
           self.stack.pop();
         },
-        Structure::ExternDecl(_) => todo!("resolve_ns externdecl"),
+        Structure::ExternDecl(decl) => {
+          self.resolve_type(&mut decl.ret)?;
+
+          for ty in decl.args.values_mut() {
+            self.resolve_type(ty)?;
+          };
+        },
       };
     };
 

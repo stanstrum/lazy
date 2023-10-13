35c05ed81f5a3577793c2806e312b4e5f6a871eb
Stan Strum <stanleystrum@gmail.com>
2023-10-12 22:38:25 -0700

    codegen: impl binary assign; fixme: why is value a pointer?

--- src/codegen/binary.rs
+++ src/codegen/binary.rs
@@ -17,13 +17,20 @@ impl<'a, 'ctx> Codegen<'a, 'ctx> {
   pub fn generate_binary_operator(&mut self, binary: &BinaryOperatorExpressionAST) -> CodeGenResult<Option<AnyValueEnum<'ctx>>> {
     match &binary.op {
       BinaryOperator::Assign => {
-        let dest = self.generate_expr(&binary.a)?;
+        let dest = self.generate_expr(&binary.a)?
+          .expect("generate_expr dest doesn't return for assign")
+          .into_pointer_value();
 
-        let value = self.generate_expr(&binary.b)?;
+        let value = self.generate_expr(&binary.b)?
+          .expect("generate_expr value doesn't return for assign")
+          .into_pointer_value();
 
-        // self.builder.build_store(, value)
+        dbg!(&dest);
+        dbg!(&value);
 
-        todo!()
+        self.builder.build_store(dest, value);
+
+        Ok(None)
       },
       _ => todo!("generate_binary_operator {:?}", &binary.op)
     }

1fc28274996b5cfbb6546f65e8549b28e3d149eb
Stan Strum <stanleystrum@gmail.com>
2024-01-23 16:32:09 -0800

    add copyright

--- src/asterizer/ast/namespace.rs
+++ src/asterizer/ast/namespace.rs
@@ -1,3 +1,10 @@
+/* Copyright (c) 2024 Stan Strum
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 use std::collections::HashMap;
 
 use crate::asterizer::{

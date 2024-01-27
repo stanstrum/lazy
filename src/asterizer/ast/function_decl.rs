46f7358cf7206015e3828ea99e0d31dbd4833df1
Stan Strum <stanleystrum@gmail.com>
2024-01-26 16:42:20 -0800

    impl FunctionDecl make

--- src/asterizer/ast/function_decl.rs
+++ src/asterizer/ast/function_decl.rs
@@ -5,6 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+use crate::asterizer::ast::FunctionDeclarationArguments;
 use crate::asterizer::{
   TokenStream,
   AsterizerError,
@@ -13,12 +14,19 @@ use crate::asterizer::{
 
 use crate::tokenizer::{
   Token,
-  TokenEnum, Operator
+  TokenEnum,
+  Operator,
+  Punctuation
 };
 
+use super::Type;
+
+#[allow(unused)]
 #[derive(Debug)]
 pub(crate) struct FunctionDeclaration {
   pub name: String,
+  return_type: Option<Type>,
+  args: Option<FunctionDeclarationArguments>,
 }
 
 impl MakeAst for FunctionDeclaration {
@@ -36,20 +44,61 @@ impl MakeAst for FunctionDeclaration {
       return Ok(None);
     };
 
-    let ident = ident.to_owned();
+    let name = ident.to_owned();
 
     stream.skip_whitespace_and_comments();
 
-    let Some(TokenEnum::Operator(Operator::RightArrow)) = stream.next_variant() else {
-      stream.pop_mark();
+    stream.push_mark();
 
-      return Ok(None);
+    let return_type = {
+      if let Some(TokenEnum::Operator(Operator::RightArrow)) = stream.next_variant() {
+        stream.skip_whitespace_and_comments();
+
+        if let Some(ty) = Type::make(stream)? {
+          stream.drop_mark();
+
+          Some(ty)
+        } else {
+          stream.pop_mark();
+
+          None
+        }
+      } else {
+        stream.pop_mark();
+
+        None
+      }
     };
 
     stream.skip_whitespace_and_comments();
 
+    stream.push_mark();
+
+    let args = {
+      if let Some(TokenEnum::Punctuation(Punctuation::Colon)) = stream.next_variant() {
+        match FunctionDeclarationArguments::make(stream)? {
+          Some(args) => {
+            stream.drop_mark();
+
+            Some(args)
+          },
+          None => {
+            stream.pop_mark();
+
+            None
+          }
+        }
+      } else {
+        stream.pop_mark();
+
+        None
+      }
+    };
+
+    stream.drop_mark();
+
     Ok(Some(Self {
-      name: ident.to_owned(),
+      name, return_type, args
     }))
   }
 }

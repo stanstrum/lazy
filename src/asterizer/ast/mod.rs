357110348e19438df9d991b6620f0e5c506e66d5
Stan Strum <stanleystrum@gmail.com>
2024-03-20 18:13:59 -0700

    remove license headers from all files; see license in LICENSE if needed

--- src/asterizer/ast/mod.rs
+++ src/asterizer/ast/mod.rs
@@ -1,10 +1,3 @@
-/* Copyright (c) 2024 Stan Strum
- * All rights reserved.
- *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 use typename::TypeName;
 
 use crate::asterizer::{

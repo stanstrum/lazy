66c4a6c7b1d09d5b95dcff5962bd854a46c51740
Stan Strum <stanleystrum@gmail.com>
2024-05-18 01:37:22 -0700

    ugly hack to fix error printing issue

--- src/tokenizer/mod.rs
+++ src/tokenizer/mod.rs
@@ -447,5 +447,18 @@ pub(crate) fn tokenize(reader: &mut Reader<File>) -> Result<Vec<Token>, Tokeniza
     };
   };
 
+  // Quick hack to fix some spans being too long due to borrower checker finnickiness
+  // -- this makes error printing a touch cleaner.  Note that the source of this problem
+  // is that we use `add_tok` in the character *after* a, say, keyword is parsed because
+  // we have to check it.  Unfortunately, the `add_tok` closure borrows `toks` and we
+  // can't append a slightly different Span manually, so here we are.
+  for token in toks.iter_mut() {
+    let Token { token: TokenEnum::Keyword(_), span } = token else {
+      continue;
+    };
+
+    span.end -= 1;
+  };
+
   Ok(toks)
 }

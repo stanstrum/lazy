0003cea3eec253523dafff9e8460a2c682e21c05
Stan Strum <stanleystrum@gmail.com>
2024-05-17 19:19:23 -0700

    move pretty print into error.rs

--- src/error.rs
+++ src/error.rs
@@ -1,7 +1,10 @@
 use snafu::prelude::*;
 
-use crate::tokenizer::TokenizationError;
 use crate::asterizer::AsterizerError;
+use crate::tokenizer::{
+  TokenizationError,
+  GetSpan
+};
 
 #[derive(Snafu, Debug)]
 #[snafu(visibility(pub(crate)))]
@@ -17,4 +20,104 @@ pub(crate) enum CompilationError {
 
   #[snafu(display("{error}"))]
   Asterization { error: AsterizerError },
+}
+
+// TODO: make this more efficient -- this can be called multiple times per
+//       error/warning/note, etc.
+fn get_line_number(source: &String, index: usize) -> usize {
+  // We humans start counting at one
+  let mut line_number = 1;
+
+  for ch in source.chars().take(index) {
+    if ch == '\n' {
+      line_number += 1;
+    };
+  };
+
+  return line_number;
+}
+
+pub(super) fn pretty_print_error(error: &AsterizerError, source: String) {
+  let span = error.get_span();
+
+  let mut focus_start = span.start;
+  let mut focus_end = span.end;
+
+  while focus_start > 0 && source.chars().nth(focus_start).unwrap() != '\n' {
+    focus_start -= 1;
+  };
+
+  while focus_end < (source.len() - 1) && source.chars().nth(focus_end).unwrap() != '\n' {
+    focus_end += 1;
+  };
+
+  focus_start += 1;
+  focus_end -= 1;
+
+  let focus_start_line_number = get_line_number(&source, focus_start);
+  let focus_end_line_number = get_line_number(&source, focus_end);
+
+  let line_number_max_digits = ((focus_end_line_number as f32).log10() + 1f32).floor() as usize;
+
+  let empty_line_number = format!("{}", " ".repeat(line_number_max_digits));
+
+  println!(" {empty_line_number} | Error: {error}", );
+  println!(" {empty_line_number} |");
+
+  let mut index = focus_start;
+  for line_number in focus_start_line_number..=focus_end_line_number {
+    print!(" {line_number: >line_number_max_digits$} | ");
+
+    let mut should_do_squiggles = false;
+
+    let line_start = index;
+    for ch in source[index..].chars() {
+      if !should_do_squiggles && index >= span.start && index <= span.end {
+        should_do_squiggles = true;
+      };
+
+      index += 1;
+
+      if matches!(ch, '\r' /* or \v, \f, \0 */) {
+        continue;
+      };
+
+      if ch == '\n' {
+        break;
+      };
+
+      print!("{ch}");
+    };
+
+    println!();
+
+    let line_length = index - line_start;
+
+    if should_do_squiggles {
+      print!(" {empty_line_number} | ");
+
+      let mut should_stop_squiggles = false;
+      for col in (line_start..).take(line_length) {
+        if col >= span.start && !should_stop_squiggles {
+          if col == span.end {
+            should_stop_squiggles = true;
+
+            break;
+          };
+
+          print!("^");
+        } else {
+          print!(" ");
+        };
+      };
+
+      if should_stop_squiggles {
+        print!(" here");
+      };
+
+      println!();
+    };
+  };
+
+  println!(" {empty_line_number} |");
 }
--- src/main.rs
+++ src/main.rs
@@ -2,115 +2,12 @@ use std::fs::File;
 
 pub(self) mod tokenizer;
 pub(self) mod asterizer;
-
 pub(self) mod error;
+
 mod debug;
 
-use asterizer::AsterizerError;
 pub(self) use error::CompilationError;
-
 use error::*;
-use tokenizer::GetSpan;
-
-// TODO: make this more efficient -- this can be called multiple times per
-//       error/warning/note, etc.
-fn get_line_number(source: &String, index: usize) -> usize {
-  // We humans start counting at one
-  let mut line_number = 1;
-
-  for ch in source.chars().take(index) {
-    if ch == '\n' {
-      line_number += 1;
-    };
-  };
-
-  return line_number;
-}
-
-fn pretty_print_error(error: &AsterizerError, source: String) {
-  let span = error.get_span();
-
-  let mut focus_start = span.start;
-  let mut focus_end = span.end;
-
-  while focus_start > 0 && source.chars().nth(focus_start).unwrap() != '\n' {
-    focus_start -= 1;
-  };
-
-  while focus_end < (source.len() - 1) && source.chars().nth(focus_end).unwrap() != '\n' {
-    focus_end += 1;
-  };
-
-  focus_start += 1;
-  focus_end -= 1;
-
-  let focus_start_line_number = get_line_number(&source, focus_start);
-  let focus_end_line_number = get_line_number(&source, focus_end);
-
-  let line_number_max_digits = ((focus_end_line_number as f32).log10() + 1f32).floor() as usize;
-
-  let empty_line_number = format!("{}", " ".repeat(line_number_max_digits));
-
-  println!(" {empty_line_number} | Error: {error}", );
-  println!(" {empty_line_number} |");
-
-  let mut index = focus_start;
-  for line_number in focus_start_line_number..=focus_end_line_number {
-    print!(" {line_number: >line_number_max_digits$} | ");
-
-    let mut should_do_squiggles = false;
-
-    let line_start = index;
-    for ch in source[index..].chars() {
-      if !should_do_squiggles && index >= span.start && index <= span.end {
-        should_do_squiggles = true;
-      };
-
-      index += 1;
-
-      if matches!(ch, '\r' /* or \v, \f, \0 */) {
-        continue;
-      };
-
-      if ch == '\n' {
-        break;
-      };
-
-      print!("{ch}");
-    };
-
-    println!();
-
-    let line_length = index - line_start;
-
-    if should_do_squiggles {
-      print!(" {empty_line_number} | ");
-
-      let mut should_stop_squiggles = false;
-      for col in (line_start..).take(line_length) {
-        if col >= span.start && !should_stop_squiggles {
-          if col == span.end {
-            should_stop_squiggles = true;
-
-            break;
-          };
-
-          print!("^");
-        } else {
-          print!(" ");
-        };
-      };
-
-      if should_stop_squiggles {
-        print!(" here");
-      };
-
-      println!();
-    };
-  };
-
-  println!(" {empty_line_number} |");
-}
 
 fn compile(args: Vec<String>) -> Result<(), CompilationError> {
   let Some(input_file_path) = args.get(1) else {

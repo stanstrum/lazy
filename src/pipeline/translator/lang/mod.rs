a271f974d8db046bfc0d5a0dad65585ec2e1c7fc
Stan Strum <stanleystrum@gmail.com>
2024-10-29 20:34:10 -0700

    ast: give a name to modules, even if they don't have one

--- src/pipeline/translator/lang/mod.rs
+++ src/pipeline/translator/lang/mod.rs
@@ -3,7 +3,11 @@ mod function;
 pub(crate) use r#type::*;
 pub(crate) use function::*;
 
-use crate::compiler::CompilerWorkflow;
+use crate::compiler::{
+  CompilerStoreHandle,
+  workflow::DefaultWorkflow,
+  CompilerWorkflow,
+};
 
 use crate::asterizer::ast;
 use crate::tokenizer::Span;
@@ -16,10 +20,24 @@ pub(crate) enum ModuleChild<W: CompilerWorkflow> {
   Module(Box<Module<W>>),
 }
 
+/// The name of a module.  This is a variant because top-level namespaces are
+/// treated the same as namespaces in files, however these top-level namespaces
+/// have no written name, so we store the Handle instead
+#[allow(unused)]
+#[derive(Debug)]
+pub(crate) enum ModuleName<W: CompilerWorkflow = DefaultWorkflow> {
+  /// This module has a name at a specific Span
+  Identifier(ast::Identifier<W>),
+  /// This module is a file's top-level namespace
+  File(CompilerStoreHandle<W>),
+}
+
 /// Represents a Module, specifically, the top-level namespace thereof
 #[allow(unused)]
 #[derive(Debug)]
 pub(crate) struct Module<W: CompilerWorkflow> {
+  /// The name of this module
+  pub(crate) name: ModuleName<W>,
   /// The children/members of this module
   pub(crate) children: Vec<ModuleChild<W>>,
   pub(crate) span: Span<W>,

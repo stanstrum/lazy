fc2af8b4d222ae969bf9dd200aadfc0f8efcab30
Stan Strum <stanleystrum@gmail.com>
2024-10-29 20:28:15 -0700

    ast: wip translate rewrite

--- src/pipeline/translator/impls/mod.rs
+++ src/pipeline/translator/impls/mod.rs
@@ -1,4 +1,4 @@
-use crate::{asterizer::ast::NamespaceChild, Result};
+use crate::Result;
 
 use crate::compiler::{
   Compiler,
@@ -31,91 +31,92 @@ impl Intrinsic {
 }
 
 impl<W: CompilerWorkflow> Translator<W> {
-  fn make_qualified_type(&self, qualified: ast::Qualified<W>) -> Result<Type<W>> {
-    // If this qualified is not explicit and only has one part, it might be an
-    // intrinsic type
-    if qualified.implicit == false && qualified.parts.len() == 1 {
-      // We know from the above check that there is exactly one element in this
-      // list.  Take that one.
-      let argument = qualified.parts.first().unwrap();
-      // Get the actual text of the identifier
-      let name = &argument.name;
+  // fn make_qualified_type(&self, qualified: ast::Qualified<W>) -> Result<Type<W>> {
+  //   // If this qualified is not explicit and only has one part, it might be an
+  //   // intrinsic type
+  //   if qualified.implicit == false && qualified.parts.len() == 1 {
+  //     // We know from the above check that there is exactly one element in this
+  //     // list.  Take that one.
+  //     let argument = qualified.parts.first().unwrap();
+  //     // Get the actual text of the identifier
+  //     let name = &argument.name;
 
-      // Check if this identifier corresponds to any Instrinsic
-      if let Some(intrinsic) = Intrinsic::try_from_slice(name) {
-        // This means that this Type can be resolved as follows
-        return Ok(Type::Intrinsic(intrinsic));
-      };
-    };
+  //     // Check if this identifier corresponds to any Instrinsic
+  //     if let Some(intrinsic) = Intrinsic::try_from_slice(name) {
+  //       // This means that this Type can be resolved as follows
+  //       return Ok(Type::Intrinsic(intrinsic));
+  //     };
+  //   };
 
-    // Otherwise, we don't recognize this reference at all
-    Ok(Type::Unresolved(UnresolvedType {
-      module: self.handle,
-      qualified,
-    }))
-  }
+  //   // Otherwise, we don't recognize this reference at all
+  //   Ok(Type::Unresolved(UnresolvedType {
+  //     module: self.handle,
+  //     qualified,
+  //   }))
+  // }
 
-  fn make_type(&self, ty: ast::Type<W>) -> Result<Type<W>> {
-    match ty {
-      ast::Type::Qualified(qualified) => self.make_qualified_type(qualified),
-    }
-  }
+  // fn make_type(&self, ty: ast::Type<W>) -> Result<Type<W>> {
+  //   match ty {
+  //     ast::Type::Qualified(qualified) => self.make_qualified_type(qualified),
+  //   }
+  // }
 
-  fn make_function_argument(&self, argument: ast::FunctionArgument<W>) -> Result<FunctionArgument<W>> {
-    Ok(FunctionArgument {
-      name: argument.identifier,
-      ty: self.make_type(argument.ty)?,
-    })
-  }
+  // fn make_function_argument(&self, argument: ast::FunctionArgument<W>) -> Result<FunctionArgument<W>> {
+  //   Ok(FunctionArgument {
+  //     name: argument.identifier,
+  //     ty: self.make_type(argument.ty)?,
+  //   })
+  // }
 
-  fn make_function(&self, _compiler: &mut Compiler<W>, function: ast::Function<W>) -> Result<Function<W>> {
-    let return_ty = match function.return_ty {
-      Some(ty) => self.make_type(ty)?,
-      None => Type::Intrinsic(Intrinsic::Void),
-    };
+  // fn make_function(&self, _compiler: &mut Compiler<W>, function: ast::Function<W>) -> Result<Function<W>> {
+  //   let return_ty = match function.return_ty {
+  //     Some(ty) => self.make_type(ty)?,
+  //     None => Type::Intrinsic(Intrinsic::Void),
+  //   };
 
-    let arguments = function.arguments
-      .map(|ast| ast.arguments)
-      .unwrap_or_default()
-      .into_iter()
-      .map(|argument| self.make_function_argument(argument))
-      .collect::<Result<_>>()?;
+  //   let arguments = function.arguments
+  //     .map(|ast| ast.arguments)
+  //     .unwrap_or_default()
+  //     .into_iter()
+  //     .map(|argument| self.make_function_argument(argument))
+  //     .collect::<Result<_>>()?;
 
-    Ok(Function {
-      name: function.identifier,
-      arguments,
-      return_ty
-    })
-  }
+  //   Ok(Function {
+  //     name: function.identifier,
+  //     arguments,
+  //     return_ty
+  //   })
+  // }
 
-  fn make_module_child(&self, compiler: &mut Compiler<W>, child: ast::NamespaceChild<W>) -> Result<ModuleChild<W>> {
-    match child {
-      NamespaceChild::Namespace(namespace) => {
-        self.make_module(compiler, *namespace)
-          .map(Box::new)
-          .map(ModuleChild::Module)
-      },
-      NamespaceChild::Function(function) => {
-        self.make_function(compiler, function)
-          .map(ModuleChild::Function)
-      },
-    }
-  }
+  // fn make_module_child(&self, compiler: &mut Compiler<W>, child: ast::NamespaceChild<W>) -> Result<ModuleChild<W>> {
+  //   match child {
+  //     ast::NamespaceChild::Namespace(namespace) => {
+  //       self.make_module(compiler, *namespace)
+  //         .map(Box::new)
+  //         .map(ModuleChild::Module)
+  //     },
+  //     ast::NamespaceChild::Function(function) => {
+  //       self.make_function(compiler, function)
+  //         .map(ModuleChild::Function)
+  //     },
+  //   }
+  // }
 
-  fn make_module(&self, _compiler: &mut Compiler<W>, _namespace: ast::Namespace<W>) -> Result<Module<W>> {
-    todo!()
-  }
+  // fn make_module(&self, _compiler: &mut Compiler<W>, _namespace: ast::Namespace<W>) -> Result<Module<W>> {
+  //   todo!()
+  // }
 
   pub(super) fn make_top_level_namespace(&self, compiler: &mut Compiler<W>, top_level: ast::TopLevelNamespace<W>) -> Result<Module<W>> {
-    let children = top_level.children.into_iter()
-      .map(
-        |child| self.make_module_child(compiler, child)
-      )
-      .collect::<Result<_>>()?;
+    todo!()
+  //   let children = top_level.children.into_iter()
+  //     .map(
+  //       |child| self.make_module_child(compiler, child)
+  //     )
+  //     .collect::<Result<_>>()?;
 
-    Ok(Module {
-      children,
-      span: top_level.span,
-    })
+  //   Ok(Module {
+  //     children,
+  //     span: top_level.span,
+  //   })
   }
 }

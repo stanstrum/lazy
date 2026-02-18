3492feff443dade9ea3ba737d20e7a9043f78cce
Stan Strum <stanleystrum@gmail.com>
2026-02-18 11:44:26 +0100

    aster: break out much of the block parsing code for both `Function` and `Block`

--- src/aster/make/expr/block.rs
+++ src/aster/make/expr/block.rs
@@ -1,9 +1,165 @@
 use std::cmp::Ordering;
+use crate::aster::make::Indenter;
 use crate::error::{Level, MessageContents, MessageSection, PrintableMessage, print_message};
 use crate::lang::reference::{ExpressionReference, Store};
+use crate::lang::span::GetSpan;
 use crate::tokenize::token::{GroupingKind, GroupingType, Operator, Span};
 
 use super::*;
+
+pub struct BlockStatement {
+  pub end: bool,
+  pub non_return_last: bool,
+  pub expr: Option<ExpressionReference>,
+}
+
+impl BlockStatement {
+  fn empty() -> Self {
+    Self {
+      end: false,
+      non_return_last: false,
+      expr: None,
+    }
+  }
+}
+
+pub fn make_block_statement<'pool, const N: usize, T: Read>(
+  lazy: &mut lang::Lazy<'pool>,
+  stream: &mut Rereader<'pool, N, T>,
+  indenter: &Indenter,
+  module: lang::reference::ModuleReference,
+  function: lang::reference::FunctionReference,
+  block: lang::reference::BlockReference,
+) -> Result<Option<BlockStatement>, Error> {
+  if let Some((Token::Indent(indent), _)) = stream.peek()? {
+    match indent.cmp(&0) {
+      Ordering::Less => {
+        stream.seek();
+        return Ok(Some(BlockStatement {
+          end: true,
+          ..BlockStatement::empty()
+        }));
+      },
+      Ordering::Equal => {
+        stream.seek();
+        return Ok(Some(BlockStatement::empty()));
+      },
+      Ordering::Greater => {
+        return Err(Error::Invalid {
+          what: line_dbg!("a newline (0 or negative indent)"),
+          at: stream.here()?,
+        });
+      },
+    };
+  };
+
+  let expr = if let Some((variable, expr)) = variable::make_assignment(lazy, stream, module, function)? {
+    let function_ref = lazy.rget(function);
+    let block_ref = lazy.rget(block);
+
+    let variable_names = block_ref.variables.iter().map(|x: &lang::expr::Variable| &x.name);
+    let argument_names = function_ref.header.arguments.iter().map(|x| &x.name);
+
+    let conflict = argument_names.chain(variable_names)
+      .find(|prior| prior.id == variable.name.id);
+
+    if let Some(conflict) = conflict {
+      print_message(lazy, PrintableMessage {
+        level: Level::Warn,
+        force: false,
+        description: "conflicting name will be shadowed".into(),
+        contents: MessageContents::WithinSource {
+          range: function_ref.span,
+          sections: vec![
+            MessageSection {
+              text: "first used here".into(),
+              span: conflict.span,
+            },
+            MessageSection {
+              text: "shadowed here".into(),
+              span: variable.name.span,
+            },
+          ],
+        },
+      });
+    };
+
+    let variable_span = variable.span;
+    let var_id = block.rget_from(lazy).variables.len();
+
+    lazy.rget_mut(block).variables.push(variable);
+
+    if let Some(b) = expr {
+      let span = b.rget_from(lazy).get_span(lazy);
+
+      let variable_reference = lang::reference::VariableReference::Block(block, var_id);
+
+      let a = function.rget_from_mut(lazy).add_expr(lang::expr::Expression::Variable { reference: variable_reference, span: variable_span });
+      let a = lang::reference::ExpressionReference(function, a);
+
+      let assignment = lang::expr::Expression::Binary {
+        a,
+        b,
+        op: (lang::expr::operator::BinaryOperator::Assign, variable_span),
+        span,
+      };
+
+      let id = function.rget_from_mut(lazy).add_expr(assignment);
+      Some(lang::reference::ExpressionReference(function, id))
+    } else {
+      None
+    }
+  } else if let Some(expr) = make_expr(lazy, stream, module, function)? {
+    Some(expr)
+  } else {
+    return stream.expected_here(line_dbg!("an expression"));
+  };
+
+  if let Some(ExpressionReference(here, id)) = expr {
+    assert!(function == here);
+    block.rget_from_mut(lazy).children.push(id);
+  };
+
+  stream.skip_whitespace_and_comments()?;
+
+  let non_return_last = {
+    if let Some((Token::Operator(Operator::Semicolon), _)) = stream.peek()? {
+      stream.seek();
+      stream.skip_whitespace_and_comments()?;
+
+      true
+    } else {
+      false
+    }
+  };
+
+  let Some((Token::Indent(indent), _)) = stream.peek()? else {
+    return stream.expected_here(line_dbg!("a newline"));
+  };
+
+  let end = match indent.cmp(&0) {
+    Ordering::Less => {
+      stream.seek();
+      true
+    },
+    Ordering::Equal => {
+      stream.seek();
+      false
+    },
+    Ordering::Greater => {
+      return Err(Error::Invalid {
+        what: line_dbg!("a newline (0 or negative indent)"),
+        at: stream.here()?,
+      });
+    },
+  };
+
+  Ok(Some(BlockStatement {
+    end,
+    non_return_last,
+    expr,
+  }))
+}
 
 pub(super) fn make_block<'pool, const N: usize, T: Read>(
   lazy: &mut lang::Lazy<'pool>,
@@ -44,107 +200,16 @@ pub(super) fn make_block<'pool, const N: usize, T: Read>(
   loop {
     stream.skip_whitespace_and_comments()?;
 
-    if let Some((Token::Indent(indent), _)) = indenter.peek(stream)? {
-      match indent.cmp(&0) {
-        Ordering::Less => {
-          stream.seek();
-          break;
-        },
-        Ordering::Equal => {
-          stream.seek();
-          continue;
-        },
-        Ordering::Greater => {
-          return Err(Error::Invalid {
-            what: line_dbg!("newline (positive indent)"),
-            at: stream.here()?,
-          });
-        },
-      };
+    let Some(stmt) = make_block_statement(lazy, stream, &indenter, module, function, block)? else {
+      return stream.expected_here(line_dbg!("a block statement"));
     };
 
-    let expr = {
-      if let Some((variable, assignment)) = variable::make_assignment(lazy, stream, module, function)? {
-        let variable_names = block.rget_from(lazy).variables.iter().map(|x: &lang::expr::Variable| &x.name);
-
-        let argument_names;
-        let range;
-
-        {
-          let function = lazy.rget(function);
-          argument_names = function.header.arguments.iter().map(|x| &x.name);
-          range = function.span;
-        };
-
-        let conflict = argument_names.chain(variable_names)
-          .find(|prior: &&lang::module::Name| prior.id == variable.name.id);
-
-        if let Some(conflict) = conflict {
-          print_message(lazy, PrintableMessage {
-            level: Level::Warn,
-            force: false,
-            description: "conflicting name will be shadowed".into(),
-            contents: MessageContents::WithinSource {
-              range,
-              sections: vec![
-                MessageSection {
-                  text: "first used here".into(),
-                  span: conflict.span,
-                },
-                MessageSection {
-                  text: "shadowed here".into(),
-                  span: variable.name.span,
-                },
-              ],
-            },
-          });
-        };
-
-        lazy.rget_mut(block).variables.push(variable);
-        assignment
-      } else if let Some(expr) = make_expr(lazy, stream, module, function)? {
-        Some(expr)
-      } else {
-        return stream.expected_here(line_dbg!("an assignment or an expression"));
-      }
+    if stmt.non_return_last && stmt.expr.is_some() {
+      non_return_last = stmt.expr;
     };
 
-    let id = if let Some(ExpressionReference(here, id)) = expr {
-      assert!(function == here);
-      lazy.rget_mut(block).children.push(id);
-      Some(id)
-    } else {
-      None
-    };
-
-    stream.skip_whitespace_and_comments()?;
-
-    if let Some((Token::Operator(Operator::Semicolon), _)) = indenter.peek(stream)? {
-      stream.seek();
-      non_return_last = id;
-
-      stream.skip_whitespace_and_comments()?;
-    };
-
-    stream.peek()?;
-    let Some((Token::Indent(indent), _)) = indenter.peek(stream)? else {
-      return stream.expected_here(line_dbg!("a newline"));
-    };
-
-    match indent.cmp(&0) {
-      Ordering::Less => {
-        stream.seek();
-        break;
-      },
-      Ordering::Equal => {
-        stream.seek();
-      },
-      Ordering::Greater => {
-        return Err(Error::Invalid {
-          what: line_dbg!("newline (positive indent)"),
-          at: stream.here()?,
-        });
-      },
+    if stmt.end {
+      break;
     };
   };
 
@@ -159,14 +224,13 @@ pub(super) fn make_block<'pool, const N: usize, T: Read>(
 
   let children = &lazy.rget(block).children;
   let returns_last = !non_return_last.is_some_and(
-    |id| id == *children.last().unwrap()
+    |ExpressionReference(_, id)| id == *children.last().unwrap()
   );
 
   let out = if returns_last {
-    let function_ref = lazy.rget(function);
-    let body = lazy.rget(function_ref.body);
-    let &index = body.children.last().unwrap();
+    let &index = children.last().unwrap();
     let reference = ExpressionReference(function, index);
+
     lang::ty::Type::Expression(reference)
   } else {
     lang::ty::Type::Intrinsic {
@@ -174,6 +238,10 @@ pub(super) fn make_block<'pool, const N: usize, T: Read>(
       span,
     }
   };
+
+  lazy.rget_mut(block).out = out;
+  lazy.rget_mut(block).returns_last = returns_last;
+  lazy.rget_mut(block).span = span;
 
   Ok(Some(block))
 }
--- src/aster/make/function.rs
+++ src/aster/make/function.rs
@@ -107,141 +107,38 @@ fn make_function_header<'pool, const N: usize, T: Read>(
 pub(super) fn make_function<'pool, const N: usize, T: Read>(
   lazy: &mut lang::Lazy<'pool>,
   stream: &mut Rereader<'pool, N, T>,
-  parent: lang::reference::ModuleReference,
+  module: lang::reference::ModuleReference,
 ) -> Result<Option<lang::reference::FunctionReference>, Error> {
-  let Some(header) = make_function_header(lazy, stream, parent)? else {
+  let Some(header) = make_function_header(lazy, stream, module)? else {
     return Ok(None);
   };
 
-  let function = lazy.create_function(parent, header);
+  let function = lazy.create_function(module, header);
   let body = lazy.rget(function).body;
   let mut non_return_last = None;
+
+  let indenter = stream.indenter_here()?;
 
   loop {
     stream.skip_whitespace_and_comments()?;
 
-    if let Some((Token::Indent(indent), _)) = stream.peek()? {
-      match indent.cmp(&0) {
-        Ordering::Less => {
-          stream.seek();
-          break;
-        },
-        Ordering::Equal => {
-          stream.seek();
-          continue;
-        },
-        Ordering::Greater => {
-          return Err(Error::Invalid {
-            what: line_dbg!("a newline (0 or negative indent)"),
-            at: stream.here()?,
-          });
-        },
-      };
+    let Some(stmt) = expr::block::make_block_statement(
+      lazy, stream, &indenter,
+      module, function, body,
+    )? else {
+      return stream.expected_here(line_dbg!("a block statement"));
     };
 
-    let expr = if let Some((variable, expr)) = expr::variable::make_assignment(lazy, stream, parent, function)? {
-      let function_ref = lazy.rget(function);
-      let body_ref = lazy.rget(body);
-
-      let variable_names = body_ref.variables.iter().map(|x: &lang::expr::Variable| &x.name);
-      let argument_names = function_ref.header.arguments.iter().map(|x| &x.name);
-
-      let conflict = argument_names.chain(variable_names)
-        .find(|prior: &&lang::module::Name| prior.id == variable.name.id);
-
-      if let Some(conflict) = conflict {
-        print_message(lazy, PrintableMessage {
-          level: Level::Warn,
-          force: false,
-          description: "conflicting name will be shadowed".into(),
-          contents: MessageContents::WithinSource {
-            range: function_ref.span,
-            sections: vec![
-              MessageSection {
-                text: "first used here".into(),
-                span: conflict.span,
-              },
-              MessageSection {
-                text: "shadowed here".into(),
-                span: variable.name.span,
-              },
-            ],
-          },
-        });
-      };
-
-      let variable_span = variable.span;
-      let var_id = body.rget_from(lazy).variables.len();
-
-      lazy.rget_mut(body).variables.push(variable);
-
-      if let Some(b) = expr {
-        let span = b.rget_from(lazy).get_span(lazy);
-
-        let variable_reference = lang::reference::VariableReference::Block(body, var_id);
-
-        let a = function.rget_from_mut(lazy).add_expr(lang::expr::Expression::Variable { reference: variable_reference, span: variable_span });
-        let a = lang::reference::ExpressionReference(function, a);
-
-        let assignment = lang::expr::Expression::Binary {
-          a,
-          b,
-          op: (lang::expr::operator::BinaryOperator::Assign, variable_span),
-          span,
-        };
-
-        let id = function.rget_from_mut(lazy).add_expr(assignment);
-        Some(lang::reference::ExpressionReference(function, id))
-      } else {
-        None
-      }
-    } else if let Some(expr) = expr::make_expr(lazy, stream, parent, function)? {
-      Some(expr)
-    } else {
-      return stream.expected_here(line_dbg!("an expression"));
+    if stmt.non_return_last && stmt.expr.is_some() {
+      non_return_last = stmt.expr;
     };
 
-    let id = if let Some(ExpressionReference(here, id)) = expr {
-      assert!(function == here);
-      body.rget_from_mut(lazy).children.push(id);
-      Some(id)
-    } else {
-      None
-    };
-
-    stream.skip_whitespace_and_comments()?;
-
-    if let Some((Token::Operator(Operator::Semicolon), _)) = stream.peek()? {
-      stream.seek();
-      stream.skip_whitespace_and_comments()?;
-
-      if id.is_some() {
-        non_return_last = id;
-      };
-    };
-
-    let Some((Token::Indent(indent), _)) = stream.peek()? else {
-      return stream.expected_here(line_dbg!("a newline"));
-    };
-
-    match indent.cmp(&0) {
-      Ordering::Less => {
-        stream.seek();
-        break;
-      },
-      Ordering::Equal => {
-        stream.seek();
-      },
-      Ordering::Greater => {
-        return Err(Error::Invalid {
-          what: line_dbg!("a newline (0 or negative indent)"),
-          at: stream.here()?,
-        });
-      },
+    if stmt.end {
+      break;
     };
   };
 
-  if let Some(ExpressionReference(_, last_id)) = function.last_expr(lazy) {
+  if let Some(last_id) = function.last_expr(lazy) {
     function.get_body_mut(lazy).returns_last = !non_return_last.is_some_and(
       |id| id == last_id
     );

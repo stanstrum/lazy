0a6ab261c7e9cb3d9e7f747cef7b4264f5105ebc
Stan Strum <stanleystrum@gmail.com>
2026-04-22 21:28:32 +0200

    wip: move code toward where it should belong

--- src/lang/lazy.rs
+++ src/lang/lazy.rs
@@ -1,14 +1,123 @@
+use std::{os::unix::fs::PermissionsExt, process::ExitCode};
+
+use crate::error;
+
 use super::*;
 
 #[derive(Debug)]
-pub struct Lazy<'a> {
-  pub(crate) pool: &'a StringPool,
+pub struct Lazy<'pool> {
+  pub(crate) pool: &'pool StringPool,
   pub(crate) pool_keys: keys::PoolKeys,
   pub(crate) settings: Settings,
   pub(crate) std: Option<ModuleReference>,
   pub(crate) modules: Vec<Module>,
   pub(crate) functions: Vec<Function>,
   pub(crate) tokens: Vec<Vec<token::TokenSpan>>,
+}
+
+impl<'pool> Lazy<'pool> {
+  pub fn check(&mut self) -> Result<ModuleReference, error::PrintableMessage> {
+    // Instantiate the global scope
+    let path = self.settings.input_path.to_owned();
+    let global = self.add_file("@global", path, None)?;
+
+    // Resolve, verify
+    crate::resolve::resolve_and_verify(self, global)?;
+
+    // Debugs
+    crate::debug::source(self, &global);
+    crate::debug::string_pool(self);
+
+    Ok(global)
+  }
+
+  pub fn build<'a>(&'a mut self) -> Result<&'a Path, error::PrintableMessage> {
+    let global = self.check()?;
+
+    // Otherwise, let's go build the module
+    let args = crate::generate::args::CliArgs {
+      target: None,
+      opt_level: crate::generate::args::OptimizationLevel::O0,
+      passes: "instcombine,reassociate,gvn,simplifycfg,mem2reg".into(),
+    };
+
+    let program = crate::generate::Program::new(global, args);
+    let compilation = program.compile(self)?;
+
+    // Debug the LLVM source
+    crate::debug::llvm_source(self, &compilation);
+
+    // Optimize the IR
+    compilation.optimize(self)?;
+    crate::debug::llvm_source(self, &compilation);
+
+    // Write out the object file for the global module
+    // TODO: get this from settings
+    let file_type = inkwell::targets::FileType::Object;
+    let object_file = compilation.save_to_file(file_type)?;
+
+    crate::debug::object_file(self, &object_file);
+
+    // TODO: find out what needs to be linked
+    let linked = [
+      "c", // libc
+    ];
+
+    // Link the module and write the program to the output file
+    let executable = object_file.link_with(&self.settings.output_path, &linked)?;
+
+    // Set the output file's permissions to be executable
+    let perms = std::fs::Permissions::from_mode(0o755);
+    std::fs::set_permissions(executable, perms)
+      .expect("failed to chmod 755 {executable:?}");
+
+    Ok(executable)
+  }
+
+  pub fn run(&mut self) -> Result<ExitCode, error::PrintableMessage> {
+    let executable = self.build()?;
+
+    // Otherwise, go run the child program
+    let mut command = std::process::Command::new(executable);
+    crate::debug::subprocess_command(self, &command);
+
+    let mut child = command
+      // .args(args);
+      .spawn()
+      .expect("to launch {executable:?}");
+
+    // Wait on the child and get an exit status
+    let exit_status = child.wait()
+      .expect("to wait on child process");
+
+    // Print that info and set our own exit code accordingly
+    let (exit_code, level, message) = if exit_status.success() {
+      (
+        ExitCode::SUCCESS, error::Level::Info,
+        "Program exited successfully.".into(),
+      )
+    } else if let Some(code) = exit_status.code() {
+      (
+        ExitCode::FAILURE, error::Level::Error,
+        format!("Program exited with status code {code}."),
+      )
+    } else {
+      (
+        ExitCode::FAILURE, error::Level::Error,
+        "Program exited unsuccessfully.".into(),
+      )
+    };
+
+    print_message!(self, {
+      level,
+      force: false,
+      description: message,
+      contents: MessageContents::None,
+    });
+
+    Ok(exit_code)
+  }
+
 }
 
 impl<'a> Lazy<'a> {
--- src/main.rs
+++ src/main.rs
@@ -12,13 +12,10 @@ mod settings;
 
 #[cfg(test)] mod test;
 
-use std::os::unix::fs::PermissionsExt;
-use std::path::Path;
 use std::process::ExitCode;
 
 pub use lang::Lazy;
 
-use crate::lang::reference::ModuleReference;
 use crate::string_pool::StringPool;
 
 fn main() -> ExitCode {
@@ -50,119 +47,17 @@ fn error_handler(
 ) -> Result<ExitCode, error::PrintableMessage> {
   match verb {
     settings::Verb::Check => {
-      check(lazy)?;
+      lazy.check()?;
 
       Ok(ExitCode::SUCCESS)
     },
     settings::Verb::Build => {
-      build(lazy)?;
+      lazy.build()?;
 
       Ok(ExitCode::SUCCESS)
     },
     settings::Verb::Run => {
-      run(lazy)
+      lazy.run()
     },
   }
-}
-
-fn check(lazy: &mut Lazy) -> Result<ModuleReference, error::PrintableMessage> {
-  // Instantiate the global scope
-  let path = lazy.settings.input_path.to_owned();
-  let global = lazy.add_file("@global", path, None)?;
-
-  // Resolve, verify
-  resolve::resolve_and_verify(lazy, global)?;
-
-  // Debugs
-  debug::source(lazy, &global);
-  debug::string_pool(lazy);
-
-  Ok(global)
-}
-
-fn build<'a>(lazy: &'a mut Lazy) -> Result<&'a Path, error::PrintableMessage> {
-  let global = check(lazy)?;
-
-  // Otherwise, let's go build the module
-  let args = generate::args::CliArgs {
-    target: None,
-    opt_level: generate::args::OptimizationLevel::O0,
-    passes: "instcombine,reassociate,gvn,simplifycfg,mem2reg".into(),
-  };
-
-  let program = generate::Program::new(global, args);
-  let compilation = program.compile(lazy)?;
-
-  // Debug the LLVM source
-  debug::llvm_source(lazy, &compilation);
-
-  // Optimize the IR
-  compilation.optimize(lazy)?;
-  debug::llvm_source(lazy, &compilation);
-
-  // Write out the object file for the global module
-  // TODO: get this from settings
-  let file_type = inkwell::targets::FileType::Object;
-  let object_file = compilation.save_to_file(file_type)?;
-
-  debug::object_file(lazy, &object_file);
-
-  // TODO: find out what needs to be linked
-  let linked = [
-    "c", // libc
-  ];
-
-  // Link the module and write the program to the output file
-  let executable = object_file.link_with(&lazy.settings.output_path, &linked)?;
-
-  // Set the output file's permissions to be executable
-  let perms = std::fs::Permissions::from_mode(0o755);
-  std::fs::set_permissions(executable, perms)
-    .expect("failed to chmod 755 {executable:?}");
-
-  Ok(executable)
-}
-
-fn run(lazy: &mut Lazy) -> Result<ExitCode, error::PrintableMessage> {
-  let executable = build(lazy)?;
-
-  // Otherwise, go run the child program
-  let mut command = std::process::Command::new(executable);
-  debug::subprocess_command(lazy, &command);
-
-  let mut child = command
-    // .args(args);
-    .spawn()
-    .expect("to launch {executable:?}");
-
-  // Wait on the child and get an exit status
-  let exit_status = child.wait()
-    .expect("to wait on child process");
-
-  // Print that info and set our own exit code accordingly
-  let (exit_code, level, message) = if exit_status.success() {
-    (
-      ExitCode::SUCCESS, error::Level::Info,
-      "Program exited successfully.".into(),
-    )
-  } else if let Some(code) = exit_status.code() {
-    (
-      ExitCode::FAILURE, error::Level::Error,
-      format!("Program exited with status code {code}."),
-    )
-  } else {
-    (
-      ExitCode::FAILURE, error::Level::Error,
-      "Program exited unsuccessfully.".into(),
-    )
-  };
-
-  print_message!(lazy, {
-    level,
-    force: false,
-    description: message,
-    contents: MessageContents::None,
-  });
-
-  Ok(exit_code)
 }

b3af6da568f29f38b0d4e612edf7dd43ccafae1a
Stan Strum <stanleystrum@gmail.com>
2026-04-24 17:24:42 +0200

    compiler: get rid of a short file

--- compiler/src/lang/mod.rs
+++ compiler/src/lang/mod.rs
@@ -3,7 +3,6 @@ pub mod module;
 pub mod function;
 pub mod ty;
 pub mod expr;
-pub mod span;
 
 use std::path::PathBuf;
 
@@ -11,4 +10,19 @@ use std::path::PathBuf;
 pub enum LazyError {
   NotExist(PathBuf),
   Aster(crate::aster::Error),
+}
+
+impl ::lang::span::GetSpan<crate::lazy::LazyStructures> for crate::lang::expr::Expression {
+  fn get_span(&self, lazy: &crate::Lazy) -> crate::tokenize::token::Span {
+    match self {
+      Self::Block(id) => id.get_span(lazy),
+      | Self::Literal { span, .. }
+      | Self::Variable { span, .. }
+      | Self::Binary { span, .. }
+      | Self::Unary { span, .. }
+      | Self::StructInitializer { span, .. }
+        => *span,
+      Self::Unknown { qualified, .. } => qualified.span,
+    }
+  }
 }
